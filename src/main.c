ba95123d9210f464f85355746eafaaeedba33eae
Jessie Pusateri <jessiepusateri@gmail.com>
2015-10-09 19:38:54 -0400

    adding window dependencies from pin example

--- src/main.c
+++ src/main.c
@@ -1,13 +1,43 @@
 #include <pebble.h>
+#include "pin_window.h"
+#include "selection_layer.h"
+
+static Window *s_main_window;
+static MenuLayer *s_menu_layer;
+static TextLayer *s_time_layer;
+
+static void pin_complete_callback(PIN pin, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Pin was %d %d %d", pin.digits[0], pin.digits[1], pin.digits[2]);
+  pin_window_pop((PinWindow*)context, true);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+}
+
+static void window_unload(Window *window) {
+  menu_layer_destroy(s_menu_layer);
+}
 
 static void init() {
-
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+      .load = window_load,
+      .unload = window_unload,
+  });
+  window_stack_push(s_main_window, true);
+  
+  //open the pin menu
+  PinWindow *pin_window = pin_window_create((PinWindowCallbacks) {
+          .pin_complete = pin_complete_callback
+        });
+  pin_window_push(pin_window, true);
 }
 
 static void deinit() {
-
+  window_destroy(s_main_window);
 }
-
+  
 int main(void) {
   init();
   app_event_loop();
